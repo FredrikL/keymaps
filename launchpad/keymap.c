ef4ece8a348a06f587f0f9adafd956d1cdb5b7be
Fredrik Leijon <fredrik.leijon@gmail.com>
2019-02-04 07:05:44 +0100

    Updated keymap with more fun stuff

--- launchpad/keymap.c
+++ launchpad/keymap.c
@@ -6,6 +6,7 @@
 extern keymap_config_t keymap_config;
 
 // Needs Unicode Hex Input enabled in MacOS
+// https://sevenseacat.net/posts/2018/unicode-in-qmk-on-osx/
 
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -21,56 +22,126 @@ extern keymap_config_t keymap_config;
 #define XXXXXXX KC_NO
 
 #define LANG_SWITCH LCTL(KC_SPC)
+#define LOCK LCTL(LGUI(KC_Q))
+#define SS LGUI(LSFT(KC_4))
 
 enum custom_keycodes {
   PLUS1,
   GRUNT,
   NODE
 };
-enum unicode_names {
-  BANG,
-  IRONY,
-  SNEK,
+
+enum unicode_name {
+  GRIN, // grinning face 😊
+  TJOY, // tears of joy 😂
+  SMILE, // grining face with smiling eyes 😁
+  HEART, // heart ❤
+  EYERT, // smiling face with heart shaped eyes 😍
+  CRY, // crying face 😭
+  SMEYE, // smiling face with smiling eyes 😊
+  UNAMU, // unamused 😒
+  KISS, // kiss 😘
+  HART2, // two hearts 💕
+  WEARY, // weary 😩
+  OKHND, // ok hand sign 👌
+  PENSV, // pensive 😔
+  SMIRK, // smirk 😏
+  RECYC, // recycle ♻
+  WINK, // wink 😉
+  THMUP, // thumb up 👍
+  THMDN, // thumb down 👎
+  PRAY, // pray 🙏
+  PHEW, // relieved 😌
+  MUSIC, // musical notes
+  FLUSH, // flushed 😳
+  CELEB, // celebration 🙌
+  CRY2, // crying face 😢
+  COOL, // smile with sunglasses 😎
+  NOEVS, // see no evil
+  NOEVH, // hear no evil
+  NOEVK, // speak no evil
+  POO, // pile of poo
+  EYES, // eyes
+  VIC, // victory hand
+  BHART, // broken heart
+  SLEEP, // sleeping face
+  SMIL2, // smiling face with open mouth & sweat
+  HUNRD, // 100
+  CONFU, // confused
+  TONGU, // face with tongue & winking eye
+  DISAP, // disappointed
+  YUMMY, // face savoring delicious food
+  CLAP, // hand clapping
+  FEAR, // face screaming in fear
+  HORNS, // smiling face with horns
+  HALO, // smiling face with halo
+  BYE, // waving hand
+  SUN, // sun
+  MOON, // moon
+  SKULL, // skull
 };
 
 const uint32_t PROGMEM unicode_map[] = {
-  [BANG]  = 0x203D,  // ‽
-  [IRONY] = 0x2E2E,  // ⸮
-  [SNEK]  = 0x1F40D, // 🐍
+  [GRIN] = 0x1F600,
+  [TJOY] = 0x1F602,
+  [SMILE] = 0x1F601,
+  [HEART] = 0x2764,
+  [EYERT] = 0x1f60d,
+  [CRY] = 0x1f62d,
+  [SMEYE] = 0x1F60A,
+  [UNAMU] = 0x1F612,
+  [KISS] = 0x1F618,
+  [HART2] = 0x1F495,
+  [WEARY] = 0x1F629,
+  [OKHND] = 0x1F44C,
+  [PENSV] = 0x1F614,
+  [SMIRK] = 0x1F60F,
+  [RECYC] = 0x267B,
+  [WINK] = 0x1F609,
+  [THMUP] = 0x1F44D,
+  [THMDN] = 0x1F44E,
+  [PRAY] = 0x1F64F,
+  [PHEW] = 0x1F60C,
+  [MUSIC] = 0x1F3B6,
+  [FLUSH] = 0x1F633,
+  [CELEB] = 0x1F64C,
+  [CRY2] = 0x1F622,
+  [COOL] = 0x1F60E,
+  [NOEVS] = 0x1F648,
+  [NOEVH] = 0x1F649,
+  [NOEVK] = 0x1F64A,
+  [POO] = 0x1F4A9,
+  [EYES] = 0x1F440,
+  [VIC] = 0x270C,
+  [BHART] = 0x1F494,
+  [SLEEP] = 0x1F634,
+  [SMIL2] = 0x1F605,
+  [HUNRD] = 0x1F4AF,
+  [CONFU] = 0x1F615,
+  [TONGU] = 0x1F61C,
+  [DISAP] = 0x1F61E,
+  [YUMMY] = 0x1F60B,
+  [CLAP] = 0x1F44F,
+  [FEAR] = 0x1F631,
+  [HORNS] = 0x1F608,
+  [HALO] = 0x1F607,
+  [BYE] = 0x1F44B,
+  [SUN] = 0x2600,
+  [MOON] = 0x1F314,
+  [SKULL] = 0x1F480,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Qwerty
-  * ,-------------.
-  * |   1  |  2   |  // UC(0x2764), \
-  * |------+------|
-  * |   3  |  4   |
-  * |------+------|
-  * |   5  |  6   |
-  * |------+------|
-  * | FUNC |  8   |
-  * `-------------'
-  */
+
   [_QWERTY] = LAYOUT( \
-      LANG_SWITCH,     X(SNEK), \
+      LANG_SWITCH,     X(SKULL), \
       GRUNT,  NODE, \
       KC_MEDIA_REWIND,      KC_MEDIA_FAST_FORWARD, \
       MO(_FUNC), PLUS1  \
   ),
 
-  /* Function
-  * ,-------------.
-  * |   Q  |CALDEL|
-  * |------+------|
-  * |   A  |TSKMGR|
-  * |------+------|
-  * |   Z  |  X   |
-  * |------+------|
-  * |      |  C   |
-  * `-------------'
-  */
   [_FUNC] = LAYOUT( \
-      X(SNEK),    KC_A, \
+      LOCK,    SS, \
       KC_A,    KC_A, \
       KC_Z,    KC_X, \
       _______, KC_C \
@@ -86,7 +157,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch (keycode) {
       case PLUS1:
-          SEND_STRING(":+1:");
+          SEND_STRING(":+1:" SS_TAP(X_ENTER));
         break;
       case GRUNT:
         SEND_STRING("grunt" SS_TAP(X_ENTER));
